3211ca4b7a0de6adb05f5e145a995820db78f5e7
Isa <iruizord@gmail.com>
2023-04-22 16:23:49 -0600

    adding variables in global scope

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
-api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid= {6a2ed7bf97664334686aa39140c421f4}
+ // variables in global scope
+ var city;
+ var cities;
+
+ var day1 = moment().add(1, "days").format("l");
+ var day2 = moment().add(2, "days").format("l");
+ var day3 = moment().add(3, "days").format("l");
+ var day4 = moment().add(4, "days").format("l");
+ var day5 = moment().add(5, "days").format("l");
+
+ var NowMoment = moment().format("l");
