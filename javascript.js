0864b9c57aa7cc8c98da3c22e81c57da7b659dce
Isa <iruizord@gmail.com>
2023-04-23 19:06:07 -0600

    adding functions to js file

--- javascript.js
+++ javascript.js
@@ -9,3 +9,48 @@
  var day5 = moment().add(5, "days").format("l");
 
  var NowMoment = moment().format("l");
+
+ function loadMostRecent() {
+    var lastSearch = localStorage.getItem("mostRecent");
+    city = lastSearch ? lastSearch : "Monterrey";
+    search();
+  }
+
+  loadMostRecent()
+
+  function loadRecentCities() {
+    const storedCities = localStorage.getItem("cities");
+    
+    if (storedCities !== null) {
+      cities = JSON.parse(storedCities);
+    } else {
+      cities = [];
+    }
+  }
+
+  loadRecentCities()
+
+  function getCity() {
+    city = $("#city-input").val();
+    if (city && cities.includes(city) === false) {
+      saveinLocalStorage();
+      return city;
+    } else if (!city) {
+      alert("Please enter a valid city");
+    }
+  }
+
+  function saveinLocalStorage() {
+    localStorage.setItem("mostRecent", city);
+    cities.push(city);
+    localStorage.setItem("cities", JSON.stringify(cities));
+  }
+
+  $("#submit").on("click", (e) => {
+    e.preventDefault();
+    getCity();
+    search();
+    $("#city-input").val("");
+    listCities();
+  });
+
